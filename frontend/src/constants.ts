2fb3d93e9158075f3ba97b8297e15d3123f86199
bigrivi <sunjianghong@gmail.com>
2025-06-07 22:06:56 +0800

    move API_URL to env

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8081/api";
+export const API_URL = import.meta.env.VITE_API_URL;
 export const TOKEN_KEY = "photinia-auth";
 export const defaultDataTimeFormat = "YYYY-MM-DD HH:mm:ss";
